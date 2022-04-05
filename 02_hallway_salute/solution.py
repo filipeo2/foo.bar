4bc6000e6b497b64f2cbc6367e277c85fcd5e655
filipeo2 <filipeo.oliveira89@gmail.com>
2022-04-05 17:36:09 -0300

    versao funcional finalizada, pendentes melhorias

--- 02_hallway_salute/solution.py
+++ 02_hallway_salute/solution.py
@@ -16,18 +16,34 @@ def solution(s):
             right_commands.append(' ')
             left_commands.append(' ')
 
-    print(right_commands)
-    print(left_commands)
+    print(f'right_commands: {right_commands}')
+    print(f'left_commands: {left_commands}')
 
-
+    # encounters from left to right
     contagem_esbarroes_direita = 0
     for i in range(0, HALLWAY_SIZE):
         if right_commands[i] == '>':
             for j in range(i, HALLWAY_SIZE):
-                if left_commands[i] == '<':
-                    contagem_esbarroes_direita = contagem_esbarroes_direita + 1            
+                if left_commands[j] == '<':
+                    contagem_esbarroes_direita += 1            
     print(f'Contagem esbarroes direita: {contagem_esbarroes_direita}')
                 
 
-solution('>----<')
-solution('<<>><')
+    # encounters from right to left
+    contagem_esbarroes_esquerda = 0
+    for i in reversed(range(0, HALLWAY_SIZE)):
+        # print('i', i)
+        if left_commands[i] == '<':
+            # print(left_commands[i])
+            for j in reversed(range(0, i)):
+                if right_commands[j] == '>':
+                    # print(right_commands[j])
+                    contagem_esbarroes_esquerda += 1            
+    print(f'Contagem esbarroes esquerda: {contagem_esbarroes_esquerda}')
+
+    return contagem_esbarroes_direita + contagem_esbarroes_esquerda
+                
+
+assert solution('>----<') == 2
+assert solution('<<>><') == 4
+assert solution('--->-><-><-->-') == 10
