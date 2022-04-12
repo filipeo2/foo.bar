e7e0293306408902830a6295a7f35368006b5d81
filipeo2 <filipeo.oliveira89@gmail.com>
2022-04-12 15:38:26 -0300

    solution 2

--- 03a_find_the_acess_codes/solution.py
+++ 03a_find_the_acess_codes/solution.py
@@ -1,23 +1,44 @@
+
+
 def solution(l):
     import itertools
     # print('\n', l)
 
-    # generate all possible combination with length 3
+    # generates all possible combination with length 3
     possible_combinations = itertools.combinations(l, 3)
 
     # check if each combination meets the requirements
     lucky_triples_amount = 0
-    for combination in possible_combinations:
-        if combination[1] % combination[0] == 0 and combination[2] % combination[1] == 0:
-            print(combination)
+    for (x, y, z) in possible_combinations:
+        if x <= y and y <= z and z % y == 0 and y % x == 0: 
+            print(x, y, z)
             lucky_triples_amount += 1
 
     return lucky_triples_amount
 
+def solution2(l):
+    from itertools import product
 
-# print('Resultado: ', solution([1, 2, 3, 4, 5, 6]))
-assert solution([1, 2, 3, 4, 5, 6]) == 3
-assert solution([1, 1, 1]) == 1
-assert solution([1, 2, 5]) == 0
-assert solution([111111, 333333, 666666, 999999]) == 2
-assert solution([]) == 0
+    print('\n', l)
+    
+    resultado = 0
+    for i in range(0, len(l)):
+        mult_menor = [l[x] for x in range(0, i) if l[x] <= l[i] and l[i] % l[x] == 0]
+        mult_maior = [l[x] for x in range(i+1, len(l)) if l[i] <= l[x] and l[x] % l[i] == 0]
+        # print(f'{l[i]}|{mult_menor} {mult_maior}')
+
+        if len(mult_menor) > 0 and len(mult_maior) > 0:
+            # print(list(product(mult_menor, mult_maior)))
+            resultado += len(list(product(mult_menor, mult_maior)))
+    
+    print(resultado)
+    return resultado
+    
+
+# print('Resultado: ', solution2([1, 2, 3, 4, 5, 6]))
+assert solution2([1, 2, 3, 4, 5, 6]) == 3
+assert solution2([1, 1, 1]) == 1
+assert solution2([1, 2, 5]) == 0
+assert solution2([111111, 333333, 666666, 999999]) == 2
+assert solution2([]) == 0
+assert solution2([1, 1, 1, 1, 1]) == 1
