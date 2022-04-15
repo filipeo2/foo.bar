b743d1bed29dd46d94cede203d86131fed1fe4d7
filipeo2 <filipeo.oliveira89@gmail.com>
2022-04-15 18:00:39 -0300

    working but wrong

--- 03b_fuel_injection_perfection/solution.py
+++ 03b_fuel_injection_perfection/solution.py
@@ -1,5 +1,6 @@
 def solution(n):
-    print('\n', n)
+    num = int(n)
+    print('\n', num)
 
     from bisect import bisect_left
 
@@ -10,36 +11,37 @@ def solution(n):
     #print((lista[-1]))
 
     # get closest index of n in the previous list (stops at next if not exists)
-    position = bisect_left(lista, n)
+    position = bisect_left(lista, num)
     print(f'position: {position} lista[]: {lista[position]}')
 
     # 0 is positive but does not exists in our list and costs 1 add_operation
-    if n in [0]:
+    if num in [0]:
         return 1
    
-    # check the current index, if is a match or what position is the closest: previous or next
-    steps_to_previous = steps_to_next = 0
-    if n == lista[position]:
+    # check the current index, if is a match
+    steps_to_closest = 0
+    if num == lista[position]:
         print(f'Achou!')
         pass
 
-    # if the previous index is closest, consider it as the base position and calculates how many add_operations are necessary
-    elif n - lista[position - 1] <= lista[position] - n:
-        steps_to_previous = n - lista[position - 1]
-        print(f'previous is the closest: {lista[position - 1]}, steps to the closest: {steps_to_previous}')
+    # if the previous index is the closest, consider it as the base position (-1) and calculates how many add_operations are necessary
+    elif num - lista[position - 1] <= lista[position] - num:
+        steps_to_closest = num - lista[position - 1]
+        print(f'previous is the closest: {lista[position - 1]}, steps to the closest: {steps_to_closest}')
         position -= 1
 
+    # if the next index is the closest, consider it as the base position and calculates how many remove_operations are necessary
     else:
-        steps_to_next = lista[position] - n
-        print(f'next is the closest: {lista[position]}, steps to the closes: {steps_to_next}')
+        steps_to_closest = lista[position] - num
+        print(f'next is the closest: {lista[position]}, steps to the closes: {steps_to_closest}')
         
-    resultado = position + steps_to_next + steps_to_previous
-    print(f'Resultado: {resultado}')
-    return resultado
+    result = position + steps_to_closest
+    print(f'Result: {result}')
+    return result
 
-assert solution(0) == 1
-assert solution(1) == 0
-assert solution(2) == 1
-assert solution(3) == 2
-assert solution(4) == 2
-assert solution(15) == 5
+assert solution('0') == 1
+assert solution('1') == 0
+assert solution('2') == 1
+assert solution('3') == 2
+assert solution('4') == 2
+assert solution('15') == 5
